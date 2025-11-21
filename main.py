d37bf55c042c07745b329b99c4344778440206c3
WizardingCat <154868985+WizardingCat@users.noreply.github.com>
2025-11-21 10:41:49 -0500

    Corrected the board checkers to exclude everything but the players as pieces to count

--- main.py
+++ main.py
@@ -15,35 +15,46 @@ def print_board(board):
 
 def check_row(n, board):
     '''returns True if the nth row is a winner'''
-    for x in range(1, len(board[n])):
-        if (board[n][0] != board[n][x]):
-            return False        
-    return True
+    if(board[0][n] == PLAYER_1 or board[0][n] == PLAYER_2):
+        for x in range(1, len(board[n])):
+            if (board[n][0] != board[n][x]):
+                return False        
+        return True
+    else:
+        return False
 
     
 def check_column(n, board):
     '''returns True if the nth column is a winner'''
-    for x in range(1, len(board)):
-        if (board[0][n] != board[x][n]):
-            return False
-    return True
-
+    if(board[0][n] == PLAYER_1 or board[0][n] == PLAYER_2):
+        for x in range(1, len(board)):
+            if (board[0][n] != board[x][n]):
+                return False
+        return True
+    else:
+        return False
 
 def check_right_diag(board):
     '''returns True if the right diagonal is a winner'''
-    for x in range(1, len(board)):
-        if (board[0][0] != board[x][x]):
-            return False
-    return True
+    if(board[0][0] == PLAYER_1 or board[0][0] == PLAYER_2):
+        for x in range(1, len(board)):
+            if (board[0][0] != board[x][x]):
+                return False
+        return True
+    else:
+        return False
+    
 
 
 def check_left_diag(board):
     '''returns True if the left diagonal is a winner'''
-    for x in range(len(board)):
-        if(board[0][len(board) - 1] != board[x][len(board) - 1 - x]):
-            return False
-    return True
-
+    if(board[0][len(board) - 1] == PLAYER_1 or board[0][len(board) - 1] == PLAYER_2):
+        for x in range(len(board)):
+            if(board[0][len(board) - 1] != board[x][len(board) - 1 - x]):
+                return False
+        return True
+    else:
+        return False
 
 def check_board(board):
     '''returns True if there is a winner''' 
@@ -72,7 +83,7 @@ def switch_player(curr_player):
 
 def main():
     print("Welcome to TicTacToe")
-    board_size = int(input("What size n do you want the nxn board to be:"))
+    board_size = int(input("What size n do you want the nxn board to be: "))
     board = []
     for x in range(board_size): 
         row = []
@@ -82,11 +93,17 @@ def main():
     print("Player 1 (X) will go first")
     curr_player = PLAYER_1
 
-    while (check_board(board)):
-        row = int(input(f"Which row do you want to enter you value player {curr_player}"))
-        column = int(input(f"Which column do you want to enter you value player {curr_player}"))
+    while (not check_board(board)):
+        column = int(input(f"Which column do you want to enter you value player {curr_player} "))
+        row = int(input(f"Which row do you want to enter you value player {curr_player} "))
         board = place_piece(row, column, board, curr_player)
-        player = switch_player(curr_player)
+        print_board(board)
+        curr_player = switch_player(curr_player)
+        print(check_board(board))
+        
+    curr_player = switch_player(curr_player)
+    print(f"Congrats {curr_player} you won!")
+
     
         
         
